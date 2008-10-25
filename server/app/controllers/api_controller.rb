39392575c6ed7557df7bc85be2c88874cd7f59f3
Federico Feroldi <pix@yahoo.it>
2008-10-25 17:33:56 +0200

    Send location of other users that last updated less than 30 minutes ago

--- server/app/controllers/api_controller.rb
+++ server/app/controllers/api_controller.rb
@@ -4,9 +4,10 @@ class ApiController < ApplicationController
     u = User.find(params[:userid])
     u.location_ln = params[:ln]
     u.location_lt = params[:lt]
+    u.location_updated_at = Time.now()
     u.save
     
-    @users = User.find(:all)
+    @users = User.find(:all, :conditions => [ 'id != ? AND location_updated_at > ?', u.id, 30.minutes.ago] )
     respond_to do |format|
       format.csv { 
         render :text => (@users.collect {|u| [u.id, u.login, u.location_lt, u.location_ln].join(",") }.join("\n")), :layout => false 
