fba314801abecc9fbbfea73aff401d64733e270f
Federico Feroldi <pix@yahoo.it>
2008-10-25 16:19:23 +0200

    Send list of users location after update

--- server/app/controllers/api_controller.rb
+++ server/app/controllers/api_controller.rb
@@ -4,8 +4,10 @@ class ApiController < ApplicationController
     u = User.find(params[:userid])
     u.location_ln = params[:ln]
     u.location_lt = params[:lt]
+    u.save
     
-    friends = User.find(:all)
+    @users = User.find(:all)
+    render :text => (@users.collect {|u| [u.id, u.login, u.location_lt, u.location_ln].join(" ") }.join("\n")), :layout => false
   end
   
 end
