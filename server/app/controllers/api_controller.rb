15d94c49b44ceac95096fac6ff2fcddc93086df8
Federico Feroldi <pix@yahoo.it>
2008-10-25 15:56:27 +0200

    Added update action

--- server/app/controllers/api_controller.rb
+++ server/app/controllers/api_controller.rb
@@ -1,2 +1,11 @@
 class ApiController < ApplicationController
+  
+  def update
+    u = User.find(params[:userid])
+    u.location_ln = params[:ln]
+    u.location_lt = params[:lt]
+    
+    friends = User.find(:all)
+  end
+  
 end
