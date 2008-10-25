5ce3a604535a81cb900b37cab558f308e4eb3673
Marco Borromeo <marco@macbook-di-marco-borromeo.local>
2008-10-25 12:51:07 +0200

    bort base schema

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -21,9 +21,9 @@ ActiveRecord::Schema.define(:version => 20080929171348) do
   end
 
   create_table "open_id_authentication_nonces", :force => true do |t|
-    t.integer "timestamp",  :null => false
+    t.integer "timestamp",                  :null => false
     t.string  "server_url"
-    t.string  "salt",       :null => false
+    t.string  "salt",       :default => "", :null => false
   end
 
   create_table "passwords", :force => true do |t|
@@ -44,14 +44,14 @@ ActiveRecord::Schema.define(:version => 20080929171348) do
   end
 
   create_table "sessions", :force => true do |t|
-    t.string   "session_id", :null => false
+    t.string   "session_id", :default => "", :null => false
     t.text     "data"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
   add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "users", :force => true do |t|
     t.string   "login",                     :limit => 40
