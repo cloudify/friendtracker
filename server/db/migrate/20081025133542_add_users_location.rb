90afc95783a296af0d1eaf7efc941fe0c8afd677
Federico Feroldi <pix@yahoo.it>
2008-10-25 15:53:46 +0200

    It's users, not user

---
+++ server/db/migrate/20081025133542_add_users_location.rb
@@ -0,0 +1,11 @@
+class AddUserLocation < ActiveRecord::Migration
+  def self.up
+    add_column :users, :location_lt, :float, :default => 0
+    add_column :users, :location_ln, :float, :default => 0
+  end
+
+  def self.down
+    remove_column :users, :location_lt
+    remove_column :users, :location_ln
+  end
+end
